77b3bf13391d207bc172d44d5ef0fb7b78ba6b92
helen-cho <hd9536@daum.net>
2023-11-24 14:33:27 +0900

    ver02

--- src/main/java/com/example/controller/ShopRestController.java
+++ src/main/java/com/example/controller/ShopRestController.java
@@ -1,14 +1,18 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dao.ShopDAO;
+import com.example.domain.QueryVO;
 import com.example.domain.ShopVO;
 import com.example.service.ShopService;
+import java.util.*;
 
 @RestController
 @RequestMapping("/shop")
@@ -22,5 +26,26 @@ public class ShopRestController {
 	@PostMapping("/insert")
 	public void insert(@RequestBody ShopVO vo) {
 		service.insert(vo);
+	}
+	
+	@PostMapping("/update")
+	public void update(@RequestBody ShopVO vo) {
+		System.out.println(vo.toString());
+		dao.update(vo);
+	}
+	
+	@GetMapping("/list.json")
+	public HashMap<String,Object> list(QueryVO vo){
+		return service.list(vo);
+	}
+	
+	@GetMapping("/delete")
+	public void delete(int pid) {
+		dao.delete(pid);
+	}
+	
+	@GetMapping("/read/{pid}")
+	public HashMap<String,Object> read(@PathVariable int pid){
+		return dao.read(pid);
 	}
 }
--- src/main/java/com/example/service/ShopServiceImpl.java
+++ src/main/java/com/example/service/ShopServiceImpl.java
@@ -1,11 +1,18 @@
 package com.example.service;
 
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.dao.ShopDAO;
+import com.example.domain.QueryVO;
 import com.example.domain.ShopVO;
+import java.io.*;
 
 @Service
 public class ShopServiceImpl implements ShopService{
@@ -17,7 +24,48 @@ public class ShopServiceImpl implements ShopService{
 	public void insert(ShopVO vo) {
 		int result=dao.check(vo.getProductId());
 		if(result==0) {
-			dao.insert(vo);
+			//이미지업로드
+			UUID uuid=UUID.randomUUID();
+			String image=uuid.toString().substring(0,8) + ".jpg";
+			try {
+				URL url=new URL(vo.getImage());
+				InputStream is=url.openStream();
+				FileOutputStream fos=new FileOutputStream("c:/upload/shop/" + image);
+				int data;
+				while((data=is.read()) != -1) {
+					fos.write(data);
+				}
+				fos.close();
+				vo.setImage("/upload/shop/" + image);
+				//데이터저장
+				dao.insert(vo);
+			}catch(Exception e) {
+				System.out.println("이미지업로드오류:" + e.toString());
+			}
 		}
 	}
+
+	@Override
+	public HashMap<String, Object> list(QueryVO vo) {
+		HashMap<String, Object> map=new HashMap<String, Object>();
+		map.put("list", dao.list(vo));
+		map.put("total", dao.total(vo));
+		return map;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
